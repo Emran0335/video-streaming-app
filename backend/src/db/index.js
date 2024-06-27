ee5f03d7c395161d7eee589b8e316e2b1aa3554a
Emran Hossain <emranhossain0335@gmail.com>
2024-06-28 00:22:02 +0600

    database connecton and environmentalvariable set up

---
+++ backend/src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js ";
+
+const connectDB = async () => {
+  try {
+    const connetionIntance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connetionIntance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MONGODB connection Failed!", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ backend/src/index.js
@@ -0,0 +1,36 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env",
+});
+
+const app = express();
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`App is listening on port ${process.env.PORT}`);
+});
+
+/*
+// if we use node module, we must use the file extention when we want to import that particular file i.e "./constants.js"
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("ERROR", error);
+    throw err;
+  }
+})();
+*/
