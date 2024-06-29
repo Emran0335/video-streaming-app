cc31261268b6d21c55d3b50f3a3a65e57dd0382f
Emran Hossain <emranhossain0335@gmail.com>
2024-06-29 10:42:30 +0600

    data model and data manipulation with jwt, bcrypt are used for the backend

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,105 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required!"],
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+    coverImage: {
+      type: String, // cludinary url
+    },
+    watchHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+// arrow function does not contain (this object) reference
+userSchema.pre("save", async function (next) {
+  // if we do not modify or create new passport, we should return next method. Otherwise, it will create new password each time as it is pre method of userSchema.
+
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// customed method created for password validation
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullName: this.fullName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    }
+  );
+};
+
+export const User = mongoose.model("User", userSchema);
+
+/*
+// users
+watchHistory ObjectId[] videos
+username string
+email string
+fullName string
+avatar string
+coverImage string
+password string
+refreshToken string
+createdAt Date
+updatedAt Date
+
+*/
---
+++ backend/src/models/video.model.js
@@ -0,0 +1,59 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      requierd: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      default: 0,
+    },
+    views: {
+      type: Number,
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model("Video", videoSchema);
+
+/*
+// videos
+
+videoFile string
+thumbnail string
+owner ObjectId users
+title string
+description string
+duration number
+views number
+isPublished boolean
+createdAt Date
+updatedAt Date
+*/
