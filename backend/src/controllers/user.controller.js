d9a306f27e7d4006d989087fb52e747d1ed5093d
Emran Hossain <emranhossain0335@gmail.com>
2024-06-30 17:35:32 +0600

    data added to the database and validation is done by postman

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,8 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
-import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -28,10 +28,21 @@ const registerUser = asyncHandler(async (req, res) => {
   if (existedUser) {
     throw new ApiError(409, "User with email or username already exists!");
   }
+  // console.log("req.files: ", req.files);
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
-  console.log(avatarLocalPath);
-  console.log(coverImageLocalPath);
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // console.log(avatarLocalPath);
+  // console.log(coverImageLocalPath);
+
+  // for checking the coverImage, it is set or not
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar is required!");
@@ -49,7 +60,7 @@ const registerUser = asyncHandler(async (req, res) => {
     coverImage: coverImage?.url || "",
     email: email,
     password: password,
-    username: username.toLowercase(),
+    username: username.toLowerCase(),
   });
 
   const createdUser = await User.findById(user._id).select(
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants.js";
-import connectDB from "./db/index.js";
-import { app } from "./app.js";
 import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: "./env",
+  path: "./.env",
 });
 
 connectDB()
