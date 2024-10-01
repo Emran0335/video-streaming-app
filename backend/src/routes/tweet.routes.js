ec25cce0684de1024c0f7d5949b26c05a29bf4d5
Emran Hossain <emranhossain0335@gmail.com>
2024-10-01 08:07:32 +0600

    tweet and channel routes are modified using middleware

--- backend/src/routes/tweet.routes.js
+++ backend/src/routes/tweet.routes.js
@@ -6,12 +6,14 @@ import {
   updateTweet,
 } from "../controllers/tweet.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { checkUser } from "../middlewares/openAuth.middleware.js";
 
 const router = Router();
-router.use(verifyJWT);
+
+router.route("/user/:userId").get(checkUser, getUserTweets);
+router.route("/").get(checkUser, getAllTweets);
 
 router.route("/").post(createTweet);
-router.route("/user/:userId").get(getUserTweets);
 router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
 
 export default router;
