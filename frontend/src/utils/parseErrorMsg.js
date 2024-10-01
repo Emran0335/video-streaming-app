f01aa7e8444754db566ad1b543faf67d6fc35ef6
Emran Hossain <emranhossain0335@gmail.com>
2024-10-01 07:43:00 +0600

    README.md file is added to describe the technologies used to develop the app

--- frontend/src/utils/parseErrorMsg.js
+++ frontend/src/utils/parseErrorMsg.js
@@ -1,18 +1,20 @@
+export const parseErrorMessage = (responseHTMLString) => {
+  const parser = new DOMParser();
+  const responseDocument = parser.parseFromString(
+    responseHTMLString,
+    "text/html"
+  );
+  const errorMessageElement = responseDocument.querySelector("pre");
 
-
-
-export const parseErrorMessage = (responseHTMLString)=> {
-    const parser = new DOMParser();
-    const responseDocument = parser.parseFromString(responseHTMLString, "text/html");
-    const errorMessageElement = responseDocument.querySelector("pre");
-
-    if(errorMessageElement) {
-        // Extract the error message usning regex
-        const errorMessage = errorMessageElement.textContent.match(/^Error:\s*(.*?)(?=\s*at)/);
-        if(errorMessage && errorMessage[1]) {
-            return errorMessage[1].trim();
-        }
+  if (errorMessageElement) {
+    // Extract the error message usning regex
+    const errorMessage = errorMessageElement.textContent.match(
+      /^Error:\s*(.*?)(?=\s*at)/
+    );
+    if (errorMessage && errorMessage[1]) {
+      return errorMessage[1].trim();
     }
+  }
 
-    return "Something went wrong"
-}
+  return "Something went wrong";
+};
