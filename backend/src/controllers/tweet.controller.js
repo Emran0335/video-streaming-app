b82ce9c4aeed366e11b19d18a0dc5b888ddb1177
Emran Hossain <emranhossain0335@gmail.com>
2024-10-10 19:07:46 +0600

    tweet controller is modified

--- backend/src/controllers/tweet.controller.js
+++ backend/src/controllers/tweet.controller.js
@@ -7,16 +7,20 @@ import mongoose, { isValidObjectId } from "mongoose";
 const createTweet = asyncHandler(async (req, res) => {
   // TODO: create tweet
   const { content } = req.body;
+
   if (!content?.trim()) {
     throw new ApiError(400, "Tweet cannot be empty");
   }
+
   const tweet = await Tweet.create({
     content,
     owner: req.user?._id,
   });
+
   if (!tweet) {
     throw new ApiError(500, "Error while adding tweet");
   }
+
   return res
     .status(201)
     .json(new ApiResponse(201, tweet, "Tweet created successfully"));
@@ -25,13 +29,52 @@ const createTweet = asyncHandler(async (req, res) => {
 const getUserTweets = asyncHandler(async (req, res) => {
   // TODO: get user tweets
   const { userId } = req.params;
+  const { page = 1, limit = 30 } = req.query;
+
   if (!userId || !isValidObjectId(userId)) {
     throw new ApiError(400, "No valid user Id found");
   }
+
   const tweets = await Tweet.aggregate([
     {
       $match: {
         owner: new mongoose.Types.ObjectId(userId),
+      },
+    },
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $skip: (page - 1) * limit,
+    },
+    {
+      $limit: parseInt(limit),
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              avatar: 1,
+              fullName: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
       },
     },
     {
@@ -47,21 +90,32 @@ const getUserTweets = asyncHandler(async (req, res) => {
         likesCount: {
           $size: "$likes",
         },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
       },
     },
     {
       $project: {
         _id: 1,
         content: 1,
+        owner: 1,
         likesCount: 1,
+        isLiked: 1,
         createdAt: 1,
         updatedAt: 1,
       },
     },
   ]);
+
   if (!tweets?.length) {
     throw new ApiError(401, "No tweets found for this user");
   }
+
   return res
     .status(200)
     .json(new ApiResponse(200, tweets, "Tweets fetched successfully"));
@@ -75,16 +129,24 @@ const updateTweet = asyncHandler(async (req, res) => {
   if (!content?.trim()) {
     throw new ApiError(400, "Tweet cannot be empty");
   }
+
   if (!tweetId || !isValidObjectId(tweetId)) {
     throw new ApiError(400, "Invalid tweet Id");
   }
+
   const tweet = await Tweet.findById(tweetId);
+
   if (!tweet) {
     throw new ApiError(500, "Tweet not found");
   }
+
   if (tweet.owner.toString() !== req.user?._id.toString()) {
-    throw new ApiError(401, "You do not have permission to update this tweet");
+    throw new ApiError(
+      401,
+      "You do not have the permission to update this tweet"
+    );
   }
+
   const updatedTweet = await Tweet.findByIdAndUpdate(
     tweetId,
     {
@@ -94,37 +156,195 @@ const updateTweet = asyncHandler(async (req, res) => {
       new: true,
     }
   );
+
   if (!updatedTweet) {
     throw new ApiError(400, "Error while updating tweet");
   }
+  const tweetWithDetails = await Tweet.aggregate([
+    {
+      $match: { _id: updatedTweet._id },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              avatar: 1,
+              fullName: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: { $first: "$owner" },
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: { $size: "$likes" },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        content: 1,
+        owner: 1,
+        likesCount: 1,
+        isLiked: 1,
+        createdAt: 1,
+        updatedAt: 1,
+      },
+    },
+  ]);
+
+  if (!tweetWithDetails.length) {
+    throw new ApiError(400, "Error while fetching updated tweet details");
+  }
+
   return res
     .status(200)
-    .json(new ApiResponse(200, updateTweet, "Tweet updated successfully"));
+    .json(
+      new ApiResponse(200, tweetWithDetails[0], "Tweet updated successfully")
+    );
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
   // TODO: delete tweet
   const { tweetId } = req.params;
+
   if (!tweetId || !isValidObjectId(tweetId)) {
     throw new ApiError(400, "Invalid tweet Id");
   }
+
   const tweet = await Tweet.findById(tweetId);
+
   if (!tweet) {
     throw new ApiError(500, "Tweet not found");
   }
+
   if (tweet.owner.toString() !== req.user?._id.toString()) {
-    throw new ApiError(401, "You do not have permission to delete this tweet");
+    throw new ApiError(
+      401,
+      "You do not the have permission to delete this tweet"
+    );
   }
+
   const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
-  
+
   if (!deletedTweet) {
     throw new ApiError(400, "Error while deleting the tweet");
   }
+
   return res
     .status(200)
     .json(new ApiResponse(200, deletedTweet, "Tweet deleted successfully"));
 });
 
-const getAllTweets = asyncHandler(async(req, res)=> {})
+const getAllTweets = asyncHandler(async (req, res) => {
+  const { page = 1, limit = 30 } = req.query;
+
+  const tweets = await Tweet.aggregate([
+    {
+      $sort: {
+        createdAt: -1,
+      },
+    },
+    {
+      $skip: (page - 1) * limit,
+    },
+    {
+      $limit: parseInt(limit),
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              _id: 1,
+              username: 1,
+              avatar: 1,
+              fullName: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "tweet",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likesCount: {
+          $size: "$likes",
+        },
+        isLiked: {
+          $cond: {
+            if: { $in: [req.user?._id, "$likes.likedBy"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        content: 1,
+        owner: 1,
+        likesCount: 1,
+        isLiked: 1,
+        createdAt: 1,
+        updatedAt: 1,
+      },
+    },
+  ]);
+
+  if (!tweets) {
+    throw new ApiError(401, "Error while fetching tweets");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, tweets, "Tweets fetched successfully"));
+});
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet, getAllTweets };
