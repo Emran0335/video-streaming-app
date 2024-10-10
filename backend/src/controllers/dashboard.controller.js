1029f0b13c1747f98e388507a1784f449fc2a6c3
Emran Hossain <emranhossain0335@gmail.com>
2024-10-10 11:20:22 +0600

    dashboard controller is modified

--- backend/src/controllers/dashboard.controller.js
+++ backend/src/controllers/dashboard.controller.js
@@ -3,7 +3,7 @@ import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 import { Tweet } from "../models/tweet.model.js";
 
 const getChannelStats = asyncHandler(async (req, res) => {
