c5e51df36bb5ee5be3a407fde03befb6d4102ca9
Emran Hossain <emranhossain0335@gmail.com>
2024-10-17 19:00:58 +0600

    LikedVideos page is complete

--- frontend/src/pages/LikedVideos.jsx
+++ frontend/src/pages/LikedVideos.jsx
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { removeUserLikedVideos } from "../store/userSlice";
 import { getUserLikedVideos } from "../hooks/getUserLikedVideos";
+import GuestLikedVideos from "../components/GuestPages/GuestLikedVideos.jsx";
+import { icons } from "../assets/Icons";
+import InfiniteScroll from "react-infinite-scroll-component";
+import VideoListCard from "../components/Video/VideoListCard";
+import GuestComponent from "../components/GuestPages/GuestComponent";
+import { BiLike } from "react-icons/bi";
 
 function LikedVideos() {
   const [loading, setLoading] = useState(true);
@@ -26,8 +32,54 @@ function LikedVideos() {
   }, [status, page]);
 
   const likedVideos = useSelector((state) => state.user.userLikedVideos);
-  console.log(likedVideos);
-  return <div>LikedVideos</div>;
+
+  const fetchMoreData = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  if (!status) {
+    return <GuestLikedVideos />;
+  }
+
+  return (
+    <>
+      {loading && (
+        <span className="flex justify-center mt-20">{icons.bigLoading}</span>
+      )}
+      {likedVideos?.length > 0 && !loading && (
+        <InfiniteScroll
+          dataLength={likedVideos.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={
+            <div className="flex justify-center h-7 mt-1">{icons.loading}</div>
+          }
+          scrollableTarget="scrollableDiv"
+        >
+          {likedVideos.map((video) => (
+            <div key={video._id}>
+              <VideoListCard
+                video={video.video}
+                imgWidth="w-[20vw]"
+                imgHeight="h-[11vw]"
+              />
+            </div>
+          ))}
+        </InfiniteScroll>
+      )}
+      {likedVideos?.length < 1 && !loading && (
+        <GuestComponent
+          icon={
+            <span className="w-full h-full flex items-center p-4 pb-5">
+              <BiLike className="w-32 h-32" />
+            </span>
+          }
+          title="Empty Liked Videos"
+          subTitle="You have no previouly liked videos"
+        />
+      )}
+    </>
+  );
 }
 
 export default LikedVideos;
