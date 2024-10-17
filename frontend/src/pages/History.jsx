1393b21f04273b4c603ad5e623b6634e1e9adf04
Emran Hossain <emranhossain0335@gmail.com>
2024-10-17 08:48:41 +0600

    pages are gonna be set

--- frontend/src/pages/History.jsx
+++ frontend/src/pages/History.jsx
@@ -6,47 +6,71 @@ import { icons } from "../assets/Icons.jsx";
 import { GoHistory } from "react-icons/go";
 import VideoListCard from "../components/Video/VideoListCard.jsx";
 import GuestComponent from "../components/GuestPages/GuestComponent.jsx";
+import { removeUserHistory } from "../store/userSlice.js";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function History() {
   const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
   const dispatch = useDispatch();
   const status = useSelector((state) => state.auth.status);
-  console.log("status", status);
+
   useEffect(() => {
     if (!status) {
-      getUserHistory(dispatch).then(() => {
+      if (page === 1) {
+        dispatch(removeUserHistory());
+      }
+      getUserHistory(dispatch, page).then((res) => {
         setLoading(false);
+        if (res.data.length !== 0) {
+          setHasMore(false);
+        }
       });
     }
-  }, [status]);
+  }, [status, page]);
 
   const history = useSelector((state) => state.user.userHistory);
-  console.log("history", history);
 
-  if (status) {
+  const fetchMoreData = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  if (!status) {
     return <GuestHistory />;
   }
   return (
     <>
       {loading && (
-        <span className="flex justify-center mt-20">{icons.BigLoading}</span>
+        <span className="flex justify-center mt-20">{icons.bigLoading}</span>
       )}
-      {history?.length > 0 &&
-        !loading &&
-        history.map((video) => (
-          <div key={video._id}>
-            <VideoListCard
-              video={video}
-              imgWidth="w-[20vw]"
-              imgHeight="h-[11vw]"
-            />
-          </div>
-        ))}
+      {history?.length > 0 && !loading && (
+        <InfiniteScroll
+          dataLength={history.length}
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={
+            <div className="flex justify-center h-7 mt-1">{icons.loading}</div>
+          }
+          scrollableTarget="scrollableDiv"
+        >
+          {history.map((video) => (
+            <div key={video._id}>
+              <VideoListCard
+                video={video}
+                imgWidth="w-[20vw]"
+                imgHeight="h-[11vw]"
+              />
+            </div>
+          ))}
+        </InfiniteScroll>
+      )}
       {history?.length < 1 && !loading && (
         <GuestComponent
           icon={
             <span className="w-full h-full flex items-center p-4 pb-5">
-              <GoHistory className="w-32 h-32"/>
+              <GoHistory className="w-32 h-32" />
             </span>
           }
           title="Empty Video History"
--- frontend/src/pages/LikedVideos.jsx
+++ frontend/src/pages/LikedVideos.jsx
@@ -1,15 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { removeUserLikedVideos } from "../store/userSlice";
+import { getUserLikedVideos } from "../hooks/getUserLikedVideos";
 
 function LikedVideos() {
-  [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [hasMore, setHasMore] = useState(true);
+
   const dispatch = useDispatch();
+  const status = useSelector((state) => state.auth.status);
 
   useEffect(() => {
-    getUserLikedVideos(dispatch).then(() => setLoading(false));
-  }, []);
+    if (status) {
+      if (page === 1) {
+        dispatch(removeUserLikedVideos());
+      }
+      getUserLikedVideos(dispatch, page).then((res) => {
+        setLoading(false);
+        if (res.data.length !== 10) {
+          setHasMore(false);
+        }
+      });
+    }
+  }, [status, page]);
 
-  const likedVideos = useSelector((state)=> state.user.userLikedVideos);
+  const likedVideos = useSelector((state) => state.user.userLikedVideos);
+  console.log(likedVideos);
   return <div>LikedVideos</div>;
 }
 
