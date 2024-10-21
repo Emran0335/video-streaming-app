91dfd145a927d140a681bfa6be0157168760cf68
Emran Hossain <emranhossain0335@gmail.com>
2024-10-21 23:38:07 +0600

    About Channel is gonna be fixed

--- frontend/src/components/Channel/AboutChannel.jsx
+++ frontend/src/components/Channel/AboutChannel.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { icons } from "../../assets/Icons";
+import formatDate from "../../utils/formatDate";
+import axiosInstance from "../../utils/axios.helper.js";
+import { MdOutlineEmail } from "react-icons/md";
+import { IoGlobeOutline, IoEyeOutline } from "react-icons/io5";
+import { BsPlayBtn } from "react-icons/bs";
+import { BiLike } from "react-icons/bi";
+import { AiOutlineMessage } from "react-icons/ai";
+import { GoInfo } from "react-icons/go";
 
 function AboutChannel() {
-  return (
-    <div>About Channel</div>
-  )
+  return <div>About Channel</div>;
 }
 
-export default AboutChannel
+export default AboutChannel;
