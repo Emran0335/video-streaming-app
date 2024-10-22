fe6547c2a623278548e50d42d9f5a68d220a692a
Emran Hossain <emranhossain0335@gmail.com>
2024-10-22 23:00:27 +0600

    AboutChannel is fixed

--- frontend/src/components/Channel/AboutChannel.jsx
+++ frontend/src/components/Channel/AboutChannel.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { icons } from "../../assets/Icons";
@@ -12,8 +12,99 @@ import { AiOutlineMessage } from "react-icons/ai";
 import { GoInfo } from "react-icons/go";
 
 function AboutChannel() {
-  
-  return <div>About Channel</div>;
+  const { username } = useParams();
+  const user = useSelector((state) => state.user.user);
+  const [aboutChannel, setAboutChannel] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const getAboutChannel = async () => {
+    try {
+      const response = await axiosInstance.get(`/dashboard/stats/${user._id}`);
+      if (response?.data?.success) {
+        setAboutChannel(response.data.data);
+      }
+    } catch (error) {
+      console.log("Error while fetching channel details", error);
+    }
+  };
+
+  useEffect(() => {
+    getAboutChannel().then(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return (
+      <span className="flex justify-center mt-20">{icons.bigLoading}</span>
+    );
+  }
+
+  return (
+    <div className="text-white px-6 py-4">
+      <div className="flex items-center m-4">
+        <h2 className="text-3xl font-semibold">@{username}</h2>
+      </div>
+      <div className="mb-4">
+        <p className="ml-1">{user.description}</p>
+      </div>
+      <div className="mb-6">
+        <h3 className="text-2xl font-semibold mb-3">Channel Details</h3>
+        <p className="ml-1 mb-[6px] flex items-center gap-2">
+          <MdOutlineEmail className="w-6 h-6" />
+          <a
+            href={`mailto:${user.email}`}
+            className="text-blue-500 hover:text-blue-400 transition-colors"
+          >
+            {user.email}
+          </a>
+        </p>
+        <p className="ml-1 mb-[6px] flex items-center gap-2">
+          <IoGlobeOutline className="w-6 h-6" />
+          <a
+            href={`/channel/${username}`}
+            className="text-blue-500 hover:text-blue-400 transition-colors"
+          >
+            {/* change the user later */}
+            {`http://localhost:5173/channel/${username}`}
+          </a>
+        </p>
+        <p className="ml-1 mb-[6px] flex items-center gap-2">
+          <BsPlayBtn className="w-6 h-6" />
+          <span>
+            <span className="font-semibold">{aboutChannel.totalVideos}</span>{" "}
+            Videos
+          </span>
+        </p>
+        <p className="ml-1 mb-[6px] flex items-center gap-2">
+          <IoEyeOutline className="w-6 h-6" />
+          <span>
+            <span className="font-semibold">{aboutChannel.totalViews}</span>{" "}
+            Views
+          </span>
+        </p>
+        <p className="ml-1 mb-[6px] flex items-center gap-2">
+          <BiLike className="w-6 h-6" />
+          <span>
+            <span className="font-semibold">{aboutChannel.totalLikes}</span>
+          </span>{" "}
+          Likes
+        </p>
+        <p className="ml-1 mb-[6px] flex items-center gap-2">
+          <AiOutlineMessage className="w-6 h-6" />
+          <span>
+            <span className="font-semibold">{aboutChannel.totalTweets}</span>{" "}
+            Tweets
+          </span>
+        </p>
+        <p className="ml-1 mb-[6px] flex items-center gap-2">
+          <GoInfo className="w-6 h-6" />
+          <span>
+            Joined on:
+            <span className="font-semibold"> {formatDate(user.createdAt)}</span>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default AboutChannel;
