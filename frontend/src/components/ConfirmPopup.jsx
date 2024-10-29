abca864720f523b6482b358a191d1bc4671d1ea5
Emran Hossain <emranhossain0335@gmail.com>
2024-10-29 19:31:38 +0600

    ConfirmPopup is gonna built

--- frontend/src/components/ConfirmPopup.jsx
+++ frontend/src/components/ConfirmPopup.jsx
@@ -1,4 +1,8 @@
 import { useState, useEffect, useImperativeHandle, useRef } from "react";
+import { useEffect, useImerativeHandle, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import { IoClose } from "react-icons/io5";
+import Button from "./Button";
 
 const ConfirmPopup = (
   {
@@ -27,7 +31,45 @@ const ConfirmPopup = (
       },
     };
   });
-  return <div>ConfirmPopup</div>;
+
+  useEffect(() => {
+    if (showPopup) {
+      dialog.current.showModal();
+    }
+  }, [showPopup]);
+
+  const handleClose = () => {
+    dialog.current.close();
+    setShowPopup(false);
+    actionFunction(false);
+  };
+
+  const handleConfirm = (event) => {
+    event.preventDefault();
+    dialog.current.close();
+    actionFunction(true);
+  };
+
+  return (
+    <div>
+      {showPopup &&
+        createPortal(
+          <dialog>
+            <div>
+              <div>
+                <form>
+                  <div>
+                    <button>
+                      <IoClose />
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </dialog>
+        )}
+    </div>
+  );
 };
 
 export default ConfirmPopup;
