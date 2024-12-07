0c7f0bc56121024236b1c820b0082e68c0f22399
emran0335 <emranhossain0335@gmail.com>
2024-12-07 13:36:20 +0600

    videoCard is set into the UI

--- frontend/src/components/Dashboard/VideoCard.jsx
+++ frontend/src/components/Dashboard/VideoCard.jsx
@@ -1,9 +1,165 @@
-import React from 'react'
+import React, { useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import formatDate from "../../utils/formatDate.js";
+import ConfirmPopup from "../ConfirmPopup.jsx";
+import axiosInstance from "../../utils/axios.helper.js";
+import { toast } from "react-toastify";
+import { updateVideoPublishStatus } from "../../store/dashboardSlice.js";
+import { deleteVideo } from "../../store/dashboardSlice.js";
+import { MdDelete, MdEdit } from "react-icons/md";
+import VideoForm from "./VideoForm.jsx";
+import { getChannelStats } from "../../hooks/getChannelStats.js";
 
-const VideoCard = () => {
+const VideoCard = ({ video }) => {
+  const dispatch = useDispatch();
+  const confirmDialog = useRef();
+  const editDialog = useRef();
+  const user = useSelector((state) => state.auth.userData);
+
+  const [publishStatus, setPublishStatus] = useState(video?.isPublished);
+
+  const handleTogglePublish = async () => {
+    try {
+      const response = await axiosInstance.patch(
+        `/videos/toggle/publish/${video._id}`
+      );
+      if (response.data.success) {
+        dispatch(
+          updateVideoPublishStatus({
+            videoId: video?._id,
+            isPublished: !video.isPublished,
+          })
+        );
+        toast.success(response.data.message);
+        setPublishStatus((pre) => !pre);
+      }
+    } catch (error) {
+      toast.error("Error while toggling publish status");
+      console.error(error);
+    }
+  };
+
+  const handleDeleteVideo = async (isConfirm) => {
+    if (isConfirm) {
+      try {
+        const response = await axiosInstance.delete(`/videos/${video._id}`);
+        if (response.data.success) {
+          toast.success(response.data.message);
+          dispatch(deleteVideo({ videoId: video._id }));
+          getChannelStats(dispatch, user._id);
+        }
+      } catch (error) {
+        toast.error("Error while deleting video");
+        console.log(error);
+      }
+    }
+  };
   return (
-    <div>VideoCard</div>
-  )
-}
+    <tr key={video._id} className="group border">
+      <td className="border-collapse border-b border-gray-600 px-4 py-3 group-last:border-none">
+        <div className="flex justify-center">
+          <label
+            htmlFor={"vid" + video._id}
+            className="relative inline-block w-12 cursor-pointer overflow-hidden"
+          >
+            <input
+              type="checkbox"
+              onClick={handleTogglePublish}
+              id={"vid" + video._id}
+              defaultChecked={video.isPublished}
+              className="peer sr-only"
+            />
+            <span className="inline-block h-6 w-full rounded-2xl bg-gray-200 duration-200 after:bottom-1 after:top-1 after:h-4 after:w-4 after:rounded-full after:bg-black after:duration-200 peer-checked:bg-pink-600 peer-checked:after:left-7"></span>
+          </label>
+        </div>
+      </td>
+      <td className="border-collapse border-b border-gray-600 px-4 py-3 group-last:border-none">
+        <div className="flex justify-center">
+          <span
+            className={`inline-block rounded-2xl border px-1.5 py-0.5 ${
+              publishStatus
+                ? "border-green-600 text-green-600"
+                : "border-orange-600 text-orange-600"
+            }`}
+          >
+            {publishStatus ? "Published" : "Unpublished"}
+          </span>
+        </div>
+      </td>
+      <td className="border-collapse border-b border-gray-600 px-4 py-3 group-last:border-none">
+        <div className="flex justify-center items-center gap-4">
+          {publishStatus ? (
+            <Link to={`/watchpage/${video._id}`}>
+              <img
+                src={video.thumbnail}
+                alt={video.title}
+                className="h-10 w-10 rounded-full object-cover"
+              />
+            </Link>
+          ) : (
+            <img
+              src={video.thumbnail}
+              alt={video.title}
+              className="h-10 w-10 rounded-full object-cover"
+            />
+          )}
+          <h3>
+            {publishStatus ? (
+              <Link
+                to={`/watchpage/${video._id}`}
+                className="hover:text-gray-300"
+              >
+                {video.title?.length > 35
+                  ? video.title.substr(0, 35) + "..."
+                  : video.title}
+              </Link>
+            ) : video.title?.length > 35 ? (
+              video.title.substr(0, 35) + "..."
+            ) : (
+              video.title
+            )}
+          </h3>
+        </div>
+      </td>
+      <td className="border-collapse border-b border-gray-600 px-4 py-3 group-last:border-none">
+        {formatDate(video.createdAt)}
+      </td>
+      <td className="border-collapse border-b border-gray-600 px-4 py-3 group-last:border-none">
+        {video.views}
+      </td>
+      <td className="border-collapse border-b border-gray-600 px-4 py-3 group-last:border-none">
+        {video.commentsCount}
+      </td>
+      <td className="border-collapse border-b border-gray-600 px-4 py-3 group-last:border-none">
+        {video.likesCount}
+      </td>
+      <td className="border-collapse border-b border-gray-600 px-4 py-3 group-last:border-none">
+        <ConfirmPopup
+          ref={confirmDialog}
+          title="Delete Video"
+          subTitle={`${video.title} - Total views: ${video.views}`}
+          confirm="Delete"
+          cancel="Cancel"
+          critical
+          message="Are you sure want to delete this video? Once deleted, you will not be able to recover it."
+          actionFunction={handleDeleteVideo}
+        />
+        <VideoForm ref={editDialog} video={video} />
+        <div className="flex justify-center gap-4">
+          <button
+            onClick={() => confirmDialog.current.open()}
+            title="Delete video"
+          >
+            <MdDelete className="h-5 w-5 hover:text-red-500" />
+          </button>
+          <button onClick={() => editDialog.current?.open()} title="Edit video">
+            <MdEdit className="h-5 w-5 hover:text-red-500" />
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
+};
 
-export default VideoCard
+export default VideoCard;
--- frontend/src/components/Dashboard/VideoPanel.jsx
+++ frontend/src/components/Dashboard/VideoPanel.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React, { useState } from "react";
+import { GoSearch } from "react-icons/go";
+import VideoCard from "./VideoCard.jsx";
 
-const VideoPanel = () => {
+const VideoPanel = ({ channelVideos }) => {
+  const [filter, setFilter] = useState(null);
+
+  function handleUserInput(input) {
+    if (!input || input === "") setFilter(channelVideos);
+    else {
+      const filteredData = channelVideos?.filter((video) =>
+        video.title.toLowerCase().startWith(input.toLowerCase().trim())
+      );
+      setFilter(filteredData);
+    }
+  }
+  let videos = filter || channelVideos;
   return (
-    <div>VideoPanel</div>
-  )
-}
+    <>
+      <div className="relative w-full mb-2 rounded-full bg-zinc-800 border py-1 pl-8 pr-3 overflow-hidden">
+        <span className="absolute left-2 top-1/2 -translate-y-1/2 text-gray-400">
+          <GoSearch />
+        </span>
+        <input
+          type="text"
+          onChange={(e) => handleUserInput(e.target.value.trim())}
+          placeholder="Search"
+          className="w-full bg-transparent outline-none"
+        />
+      </div>
 
-export default VideoPanel
+      <div className="w-full overflow-auto">
+        <table className="w-full min-w-[1000px] border-collapse border text-white">
+          <thead>
+            <tr>
+              <th className="border-collapse border-b p-4">Toggle</th>
+              <th className="border-collapse border-b p-4">Status</th>
+              <th className="border-collapse border-b p-4">Video</th>
+              <th className="border-collapse border-b p-4">Date Upload</th>
+              <th className="border-collapse border-b p-4">Views</th>
+              <th className="border-collapse border-b p-4">Comments</th>
+              <th className="border-collapse border-b p-4">Likes</th>
+              <th className="border-collapse border-b p-4">Options</th>
+            </tr>
+          </thead>
+          <tbody>
+            {videos?.map((video) => (
+              <VideoCard key={video._id} video={video} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+
+export default VideoPanel;
