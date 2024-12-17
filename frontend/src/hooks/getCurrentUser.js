ba1cbaa99600c7e17e07c7aa67fd090959121fe8
emran0335 <emranhossain0335@gmail.com>
2024-12-17 12:55:16 +0600

    getCurrent and heathCheck are done

--- frontend/src/hooks/getCurrentUser.js
+++ frontend/src/hooks/getCurrentUser.js
@@ -5,7 +5,7 @@ export const getCurrentUser = async (dispatch) => {
   try {
     const response = await axiosInstance.get("/users/current-user");
     if (response?.data?.data) {
-      dispatch(setUser(response.data.data));
+      dispatch(setUser(response.data?.data));
       return response.data;
     }
   } catch (error) {
--- frontend/src/hooks/healthCheck.js
+++ frontend/src/hooks/healthCheck.js
@@ -4,7 +4,7 @@ import { toast } from "react-toastify";
 export const healthCheck = async () => {
   try {
     const response = await axiosInstance.get("/healthcheck");
-    return response.data.data;
+    return response.data?.data;
   } catch (error) {
     toast.error("Oops! Server is not working");
     console.log(error);
