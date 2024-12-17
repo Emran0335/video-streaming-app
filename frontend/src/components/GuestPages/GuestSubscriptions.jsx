78ca263796749f123200deb8f46cb3b08e3bd20c
emran0335 <emranhossain0335@gmail.com>
2024-12-17 13:47:15 +0600

    GuestSubscription is added to the UI

--- frontend/src/components/GuestPages/GuestSubscriptions.jsx
+++ frontend/src/components/GuestPages/GuestSubscriptions.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import GuestComponent from './GuestComponent.js';
-
+import React from "react";
+import GuestComponent from "./GuestComponent.jsx";
+import { FaRegPlayCircle } from "react-icons/fa";
 
 const GuestSubscriptions = () => {
   return (
-    <GuestComponent 
-    title='Watch videos you like'
-    subTitle="Login and get the videos of channel you subscribe at one place"
+    <GuestComponent
+      title="Watch videos you like"
+      subTitle="Login and get the videos of channel you subscribe at one place"
+      icon={
+        <span className="w-full h-full flex items-center p-4 pb-5">
+          <FaRegPlayCircle className="w-32 h-32" />
+        </span>
+      }
+      route="/subscriptions"
     />
-  )
-}
+  );
+};
 
-export default GuestSubscriptions
+export default GuestSubscriptions;
